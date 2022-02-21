eae57ef3a681e795f0057a00d39af314259a57c0
Mikkel <mvtklooster@hotmail.com>
2022-02-21 14:03:10 +0100

    Use malloc for flag

--- gamelog.c
+++ gamelog.c
@@ -2,8 +2,6 @@
 #include <getopt.h>
 #include <stdlib.h>
 
-void parseGlobalOptions(int *opt, int argc, char *argv[]);
-
 int main(int argc, char *argv[]) {
 
     // Ensure command is given
@@ -12,51 +10,42 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    // Initialise variable for index
     int option_index = 0;
 
+    // Free memory for flag
     int *opt = malloc(sizeof(char));
     if (opt == NULL) {
         printf("Could not allocate memory for option\n");
-        return 1;
+        return 2;
     }
 
     // Create command options
     struct option long_options[] = {
         {"add", required_argument, opt, 'a'},
         {"remove", required_argument, opt, 'r'},
-        {"move", required_argument, opt, 'm'},
+        {"update", required_argument, opt, 'u'},
         {"list", optional_argument, opt, 'l'},
+        {"version", no_argument, opt, 'v'},
+        {"help", no_argument, opt, 'h'},
         {0, 0, 0, 0},
     };
 
-    getopt_long(argc, argv, "armu", long_options, &option_index);
+    // Parse option
+    getopt_long(argc, argv, "a:r:u:l::", long_options, &option_index);
 
     // Ensure option is valid
     if (*opt == '?') {
         printf("Invalid option\n");
         free(opt);
-        return 2;
-    }
-
-    // Ensure only 1 option entered
-    if (getopt_long(argc, argv, "armu", long_options, &option_index) != -1) {
-        printf("Only one option allowed\n");
-        free(opt);
         return 3;
     }
 
-    // Ensure only 2 arguments when moving
-    if (*opt == 'm' && argc != optind + 1) {
-        printf("Only two arguments allowed\n");
+    // Ensure only 1 option entered
+    if (getopt_long(argc, argv, "a:r:u:l::", long_options, &option_index) != -1) {
+        printf("Only one option allowed\n");
         free(opt);
         return 4;
-    }
-
-    // Ensure max 1 argument for the rest
-    else if (*opt != 'm' && argc != optind) {
-        printf("Only one argument allowed\n");
-        free(opt);
-        return 5;
     }
 
     switch(*opt) {
@@ -71,45 +60,28 @@ int main(int argc, char *argv[]) {
             printf("Option --remove selected\n");
             break;
         
-        // Move game
-        case 'm':
-            printf("Option --move selected\n");
+        // update game
+        case 'u':
+            printf("Option --update selected\n");
             break;
 
         // Show list of games
         case 'l':
             printf("Option --list selected\n");
             break;
+
+        // Show version
+        case 'v':
+            printf("Option --version selected\n");
+            break;
+
+        // Show help
+        case 'h':
+            printf("Option --help selected\n");
+            break;
     }
 
-    parseGlobalOptions(opt, argc, argv);
     free(opt);
     return 0;
 }
 
-void parseGlobalOptions(int *opt, int argc, char *argv[]) {
-    struct option long_options[] = {
-        {"help", no_argument, opt, 'h'},
-        {"version", no_argument, opt, 'v'},
-    };
-
-    int option_index = 0;
-    getopt_long(argc, argv, "", long_options, &option_index);
-
-    if (*opt == '?') {
-        printf("Invalid option\n"); 
-    }
-
-    if (getopt_long(argc, argv, "", long_options, &option_index) != -1) {
-        printf("Only one option allowed\n");
-    }
-
-    switch (*opt) {
-        case 'h':
-            printf("Option --help selected\n");
-            break;
-        case 'v':
-            printf("Option --version selected\n");
-            break;
-    }
-}
